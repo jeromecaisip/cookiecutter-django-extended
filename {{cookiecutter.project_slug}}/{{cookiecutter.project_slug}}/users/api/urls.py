f779a54990869afe2c742e4ab71a26d85aa36515
Daniel Sears <Daniel.Sears@gmail.com>
2019-09-17 00:15:16 -0700

    moved config/api_router.py to users/api/urls.py to consolidate users app in one directory structure

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from django.conf import settings
+from {{ cookiecutter.project_slug }}.users.api.views import UserViewSet
+
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
+
+router.register('users', UserViewSet)
+
+urlpatterns = router.urls
