758202404385affc85217ebf90b825b06bbbd871
Jerome Caisip <jerome.caisip@gmail.com>
2019-12-26 03:29:26 +0800

    Fix duplicate drf statement.

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -316,19 +316,6 @@ REST_FRAMEWORK = {
     )
 }
 {%- endif %}
-
-{% if cookiecutter.use_drf == "y" -%}
-# django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
-}
-{%- endif %}
 {% if cookiecutter.use_behave_django == 'y' -%}
 INSTALLED_APPS +=["behave_django"]
 {%- endif%}
